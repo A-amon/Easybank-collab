7970169853618c8c2e370aa46b7eadf8480cbfc3
A-amon <64846892+A-amon@users.noreply.github.com>
2021-09-18 15:21:59 +0800

    Add why section

---
+++ index.js
@@ -0,0 +1,28 @@
+import { setIntersectionObserver } from './modules/intersectionObserver.js'
+
+/**
+ * Observe .why-card elements intersection
+ */
+const observeWhy = () => {
+	const whyCards = document.querySelectorAll('.why-card')
+
+	setIntersectionObserver(whyCards, onWhyEnter, onWhyExit)
+}
+
+/**
+ * Handle .why-card is intersecting
+ * @param {object} item 
+ */
+const onWhyEnter = (item) => {
+	item.classList.add('anim')
+}
+
+/**
+ * Handle .why-card is not intersecting
+ * @param {object} item 
+ */
+const onWhyExit = (item) => {
+	item.classList.remove('anim')
+}
+
+observeWhy()
