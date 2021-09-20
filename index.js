e11acdc8b1d0f2e09988b6bddb3749a52df07743
BHD-Ilyes <ilyeshajdahman81@gmail.com>
2021-09-20 20:43:07 +0100

    � put css instead of js to set the green color when hover

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-let fb = document.getElementById("fb");
-fb.addEventListener("mouseover", () => {
-  fb.src = "assets/icons/icon-facebook-green.svg";
-});
-fb.addEventListener("mouseout", () => {
-  fb.src = "assets/icons/icon-facebook.svg";
-});
 
-let yt = document.getElementById("yt");
-yt.addEventListener("mouseover", () => {
-  yt.src = "assets/icons/icon-youtube-green.svg";
-});
-yt.addEventListener("mouseout", () => {
-  yt.src = "assets/icons/icon-youtube.svg";
-});
-
-let tw = document.getElementById("tw");
-tw.addEventListener("mouseover", () => {
-  tw.src = "assets/icons/icon-twitter-green.svg";
-});
-tw.addEventListener("mouseout", () => {
-  tw.src = "assets/icons/icon-twitter.svg";
-});
-
-let pt = document.getElementById("pt");
-pt.addEventListener("mouseover", () => {
-  pt.src = "assets/icons/icon-pinterest-green.svg";
-});
-pt.addEventListener("mouseout", () => {
-  pt.src = "assets/icons/icon-pinterest.svg";
-});
-
-let ig = document.getElementById("ig");
-ig.addEventListener("mouseover", () => {
-  ig.src = "assets/icons/icon-instagram-green.svg";
-});
-ig.addEventListener("mouseout", () => {
-  ig.src = "assets/icons/icon-instagram.svg";
-});
 import { setIntersectionObserver } from './modules/intersectionObserver.js'
 
 /**
