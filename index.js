fc24d6ed1bd9756411369e671b9433781a256a3c
simretB05 <happysimret@gmai.com>
2021-10-04 11:52:18 -0600

    update hero section

--- index.js
+++ index.js
@@ -2,24 +2,25 @@
 const navbarMenu = document.querySelector('.navbar__menu')
 const navbarList = document.querySelector('.navbar__list')
 const hamburger = document.querySelector('.navbar__menu-hamburger')
-const mockUp = document.querySelector('.hero__mockups')
+const mockUp = document.querySelector('.hero__img')
 const overlay = document.querySelector('.overlay')
-const hero = document.querySelector('.hero-section')
+const hero = document.querySelector('.hero')
 const close = document.querySelector('.navbar__menu-close')
+
 navbarMenu.addEventListener('click', () => {
     navbarList.classList.toggle("toggle");
     hamburger.classList.toggle("hide");
     navbarList.classList.toggle("hide");
     close.classList.toggle("change")
-    mockUp.classList.toggle('hide');
-    // hero.classList.toggle('overlay');
+    mockUp.classList.toggle("hide");
+    hero.classList.toggle('overlay');
 
     toggleScroll();
 
 });
+
 //  *stop-scroll/
 const toggleScroll = () => {
     document.body.classList.toggle('hide-scroll')
-
 
 }
